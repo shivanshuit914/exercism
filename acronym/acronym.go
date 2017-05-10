1ed8ecff7371fb2f3526169210b26dc91a93d7ce
Shivanshu Patel <shivanshu.patel@gohenry.co.uk>
2017-05-10 12:15:58 +0100

    acronym WIP

--- acronym/acronym.go
+++ acronym/acronym.go
@@ -9,18 +9,20 @@ import (
 const testVersion = 2
 
 func Abbreviate(inputString string) string {
-	// var returnstring string
+	var returnstring string
 
 	inputString = strings.Title(inputString)
 
-	reg, err := regexp.Compile("[a-z0-9,:]")
+	reg, err := regexp.Compile("[a-z0-9,:-]")
 	if err != nil {
 		log.Fatal(err)
 	}
-	// splitted := strings.Split(inputString, " ")
-	// for _, value := range splitted {
-	// 	returnstring = returnstring + value[0:1]
-	// }
+	inputString = strings.Replace(inputString, "-", " ", 1)
+	inputString = reg.ReplaceAllString(inputString, "")
+	splitted := strings.Split(inputString, " ")
+	for _, value := range splitted {
+		returnstring = returnstring + value[0:1]
+	}
 
-	return reg.ReplaceAllString(inputString, "")
+	return returnstring
 }
